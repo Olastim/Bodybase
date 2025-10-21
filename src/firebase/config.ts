511a2c638aea38028862d86a94218244465a6436
Timothy Olamideji <timothyolamideji@gmail.com>
2025-10-21 01:22:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2373975833-6c94a",
+  "appId": "1:306088436143:web:63fcf5cf91cb329aa64f5a",
+  "apiKey": "AIzaSyB4SDqjzZOvSz2ziEhjO1UkWOvTuPreK48",
+  "authDomain": "studio-2373975833-6c94a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "306088436143"
+};
